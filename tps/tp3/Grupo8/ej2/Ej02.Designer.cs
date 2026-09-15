1d0cd65af3ed7418880e4c28d87f88df38408621
valenbarberi <valen.barberi8@gmail.com>
2026-09-15 18:03:31 -0300

    ej2

---
+++ tps/tp3/Grupo8/ej2/Ej02.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace TP3
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox_numero1 = new TextBox();
+            textBox_numero2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label_resultado = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox_numero1
+            // 
+            textBox_numero1.Location = new Point(190, 119);
+            textBox_numero1.Name = "textBox_numero1";
+            textBox_numero1.Size = new Size(100, 23);
+            textBox_numero1.TabIndex = 0;
+            // 
+            // textBox_numero2
+            // 
+            textBox_numero2.Location = new Point(450, 119);
+            textBox_numero2.Name = "textBox_numero2";
+            textBox_numero2.Size = new Size(100, 23);
+            textBox_numero2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(170, 101);
+            label1.Name = "label1";
+            label1.Size = new Size(144, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Ingresar el primer numero";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(427, 101);
+            label2.Name = "label2";
+            label2.Size = new Size(155, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Ingresar el segundo numero";
+            // 
+            // label_resultado
+            // 
+            label_resultado.AutoSize = true;
+            label_resultado.Location = new Point(335, 236);
+            label_resultado.Name = "label_resultado";
+            label_resultado.Size = new Size(0, 15);
+            label_resultado.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(190, 228);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 6;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label_resultado);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox_numero2);
+            Controls.Add(textBox_numero1);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox_numero1;
+        private TextBox textBox_numero2;
+        private Label label1;
+        private Label label2;
+        private Label label_resultado;
+        private Button button1;
+    }
+}
---
+++ tps/tp3/Grupo8/ej2/Ej02.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TP3
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // This method is called when the form loads. You can add any initialization code here if needed.
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label_resultado.Text= "El resultado de la suma es " + (Convert.ToInt32(textBox_numero1.Text) + Convert.ToInt32(textBox_numero2.Text));
+        }
+    }
+
+
+}
