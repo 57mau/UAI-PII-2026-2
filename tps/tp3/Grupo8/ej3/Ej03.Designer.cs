d9cbfe6674c0ab259d8848257cab2b84b49b792b
valenbarberi <valen.barberi8@gmail.com>
2026-09-15 18:04:34 -0300

    ej3

---
+++ tps/tp3/Grupo8/ej3/Ej03.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace TP3
+{
+    partial class Ej03
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(325, 200);
+            button1.Name = "button1";
+            button1.Size = new Size(133, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Ingresar Numeros";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(342, 171);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(307, 153);
+            label1.Name = "label1";
+            label1.Size = new Size(184, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Cuantos numeros desea ingresar?";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(307, 226);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 15);
+            label2.TabIndex = 3;
+            // 
+            // Ej3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Ej3";
+            Text = "Ej3";
+            Load += Ej3_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ tps/tp3/Grupo8/ej3/Ej03.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TP3
+{
+    public partial class Ej03 : Form
+    {
+        public Ej03()
+        {
+            InitializeComponent();
+        }
+
+        private void Ej3_Load(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             int cantidad = Convert.ToInt32(textBox1.Text);
+            int suma = 0;
+            string listanumeros = "";
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                string entrada = Interaction.InputBox("Ingrese el numero " + (i + 1) + ":", "Carga de numeros");
+                int num = Convert.ToInt32(entrada);
+                suma += num;
+                listanumeros += num + "- ";
+            }
+
+            textBox1.Text = listanumeros;
+            label2.Text= "Suma total: " + suma;
+        }
+    }
+}
