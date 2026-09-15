a73efa224378b6561be9ac298a8dbe2ad204d71e
valenbarberi <valen.barberi8@gmail.com>
2026-09-15 18:56:59 -0300

    ej9

---
+++ tps/tp3/Grupo8/ej9/Ej09.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TP3
+{
+    partial class Ej09
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnDados = new Button();
+            dataGridViewResultados = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewResultados).BeginInit();
+            SuspendLayout();
+            // 
+            // btnDados
+            // 
+            btnDados.Location = new Point(210, 92);
+            btnDados.Name = "btnDados";
+            btnDados.Size = new Size(75, 23);
+            btnDados.TabIndex = 0;
+            btnDados.Text = "Tirar Dados";
+            btnDados.UseVisualStyleBackColor = true;
+            btnDados.Click += btnDados_Click;
+            // 
+            // dataGridViewResultados
+            // 
+            dataGridViewResultados.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewResultados.Location = new Point(445, 60);
+            dataGridViewResultados.Name = "dataGridViewResultados";
+            dataGridViewResultados.Size = new Size(240, 150);
+            dataGridViewResultados.TabIndex = 1;
+            // 
+            // Ej9
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridViewResultados);
+            Controls.Add(btnDados);
+            Name = "Ej9";
+            Text = "Ej9";
+            Load += Ej9_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridViewResultados).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnDados;
+        private DataGridView dataGridViewResultados;
+    }
+}
---
+++ tps/tp3/Grupo8/ej9/Ej09.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TP3
+{
+    public partial class Ej09 : Form
+    {
+        int[] conteo = new int[7];
+        Random random = new Random();
+        int totalNumerosTirados = 0;
+        public Ej09()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDados_Click(object sender, EventArgs e)
+        {
+           
+            int dado1 = random.Next(1, 7);
+            int dado2 = random.Next(1, 7);
+            conteo[dado1]++;
+            conteo[dado2]++;
+            totalNumerosTirados += 2;
+
+            for (int i = 1; i <= 6; i++)
+            {
+                int indice_fila = i - 1; 
+                int cantidad = conteo[i];
+                double porcentaje = ((double)cantidad / totalNumerosTirados) * 100.0;
+
+                dataGridViewResultados.Rows[indice_fila].Cells[1].Value = cantidad;
+                dataGridViewResultados.Rows[indice_fila].Cells[2].Value = porcentaje.ToString("0.00") + " %";
+            }
+
+        }
+
+
+        private void Ej9_Load(object sender, EventArgs e)
+        {
+            dataGridViewResultados.Columns.Add("colNumero", "Número");
+            dataGridViewResultados.Columns.Add("colCantidad", "Cantidad");
+            dataGridViewResultados.Columns.Add("colPorcentaje", "Porcentaje (%)");
+
+            for (int i = 1; i <= 6; i++)
+            {
+                dataGridViewResultados.Rows.Add(i, 0, "0.00 %");
+            }
+        }
+    }
+
+}
