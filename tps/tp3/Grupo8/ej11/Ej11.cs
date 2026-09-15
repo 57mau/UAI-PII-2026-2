aedd399d62ea14173d036bc8248f8308fb49a597
valenbarberi <valen.barberi8@gmail.com>
2026-09-15 18:59:50 -0300

    ej11

---
+++ tps/tp3/Grupo8/ej11/Ej11.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TP3
+{
+    public partial class eJ11 : Form
+    {
+        public eJ11()
+        {
+            InitializeComponent();
+        }
+    }
+}
